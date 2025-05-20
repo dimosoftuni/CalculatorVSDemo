64ec80af15dd1d9aa7835ae85d85f1ed8d472ffe
Dimo Mitev <dimosoftuni@gmail.com>
2025-05-20 21:08:55 +0300

    some change 2

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 
     // Subtracts second number from first
     public double subtract(double a, double b) {
-        return a - b - f;
+        return a - b - f - fff;
     }
 
     // Multiplies two numbers
